c489fce35ad8ff852a6460062434cd208b20094a
Vlad Usikov <v.a.usikov@yandex.ru>
2024-04-12 00:38:41 +0300

    readme update

--- conftest.py
+++ conftest.py
@@ -5,7 +5,7 @@ def record_adb_logcat():
     """
     Эта фикстура неявно записывает логкат во время всей тестовой сессии.
     """
-
+    subprocess.run('adb logcat -c', shell=True)
     logcat_process = subprocess.Popen('adb logcat > logs.txt', shell=True)
     yield
     logcat_process.kill()
