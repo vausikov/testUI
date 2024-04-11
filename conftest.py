e31213039f52cec183ca984559c2b09f8f0828a8
Vlad Usikov <v.a.usikov@yandex.ru>
2024-04-12 00:04:02 +0300

    add conftest and fixture example

---
+++ conftest.py
@@ -0,0 +1,11 @@
+import pytest
+import subprocess
+@pytest.fixture(autouse=True, scope='session')
+def record_adb_logcat():
+    """
+    Эта фикстура неявно записывает логкат во время всей тестовой сессии.
+    """
+
+    logcat_process = subprocess.Popen('adb logcat > logs.txt', shell=True)
+    yield
+    logcat_process.kill()
